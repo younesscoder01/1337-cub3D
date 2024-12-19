14f61dc88bd7ba0e54d19d6fdaeaa3152907a720
younesscoder01 <younesscoder@gmail.com>
2024-12-19 10:18:14 +0100

    add the initial structs

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ysahraou <ysahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 09:29:47 by ysahraou          #+#    #+#             */
-/*   Updated: 2024/12/19 09:30:08 by ysahraou         ###   ########.fr       */
+/*   Updated: 2024/12/19 10:17:34 by ysahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,50 @@
 # define CUB3D_H
 
 # include <stdio.h>
+
+//events
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+//player struct
+typedef struct s_player
+{
+	int x;
+	int y;
+	double radius;
+	double turnDirection;
+	double walkDirection;
+	double rotationAngle;
+	double moveSpeed;
+	double rotationSpeed;
+}				t_player;
+
+//img struct
+typedef struct	s_img_info {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+    size_t img_height;
+    size_t img_width;
+}				t_img_info;
+
+//data struct that containing all the data
+typedef struct  s_data
+{
+    void	*mlx;
+    void	*mlx_win;
+	t_player player;
+	t_img_info minimap;
+	char **map;
+}               t_data;
 
 #endif
